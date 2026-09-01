17220ba25ee93178b36a1e708b8f26a4b4a9b23d
Metaverse0909 <msboyindiain@gmail.com>
2026-09-01 16:17:36 +0530

    Add Ledvix splash screen and local WebView

--- android-project/app/src/main/java/com/metaverse/expensetracker/MainActivity.kt
+++ android-project/app/src/main/java/com/metaverse/expensetracker/MainActivity.kt
@@ -2,14 +2,30 @@ package com.metaverse.expensetracker
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
 
+    private val splashDuration = 1500L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        showSplash()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            showApp()
+        }, splashDuration)
+    }
+
+    private fun showSplash() {
+        setContentView(R.layout.splash_screen)
+    }
+
+    private fun showApp() {
         setContentView(R.layout.activity_main)
 
         val webView = findViewById<WebView>(R.id.webView)
@@ -22,5 +38,15 @@ class MainActivity : Activity() {
         webView.webViewClient = WebViewClient()
 
         webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        val webView = findViewById<WebView>(R.id.webView)
+
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
